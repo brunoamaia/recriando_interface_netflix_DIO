a97942d3a331fd1d1823b449828ab50c703ede5c
brunoamaia <brunoamaia94@gmail.com>
2020-11-22 11:31:37 -0300

    área para trailer

--- pages/about/script.js
+++ pages/about/script.js
@@ -2,6 +2,13 @@
 const id = sessionStorage.getItem("movie");
 
 
+let trailer = document.querySelector(".trailer")
+
+trailer.innerHTML = `<iframe id="ytplayer" type="text/html" width="280" height="158" 
+  src="http://www.youtube.com/embed/${dataFake[id].trailler}"
+  frameborder="0" controls=0 >
+`
+
 console.log('Inicio')
 console.log(`filme = ${id}`)
 console.log(`data fake = ${dataFake[0].trailler}`)
